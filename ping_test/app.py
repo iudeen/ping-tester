4eace71f53f836b42919eeb442e7d1ab33bb5a07
Irfanuddin Shafi Ahmed <irfanudeen08@gmail.com>
2023-07-05 16:53:34 +0530

    Initial commit

---
+++ ping_test/app.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+
+from asyncer import asyncify
+from fastapi import FastAPI, WebSocket, WebSocketException, Query, Depends
+from fastapi import status
+
+from ping_test import ping_tester
+
+app = FastAPI(title="Ping Tester")
+
+
+async def get_configuration(
+        target: Annotated[str | None, Query()] = None,
+        interval: Annotated[int, Query()] = 0,
+        timeout: Annotated[int, Query()] = 2,
+        count: Annotated[int, Query()] = 4,
+):
+    if not target:
+        raise WebSocketException(status.WS_1007_INVALID_FRAME_PAYLOAD_DATA, reason="Target required")
+    return {
+        "target": target, "interval": interval, "timeout": timeout, "count": count
+    }
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket, config: dict = Depends(get_configuration)):
+    try:
+        await websocket.accept()
+        message = await asyncify(ping_tester)(**config)
+        for i in message:
+            await websocket.send_text(i.__repr__())
+        await websocket.close()
+    except Exception as e:
+        await websocket.close(status.WS_1003_UNSUPPORTED_DATA, reason=e.__repr__())
